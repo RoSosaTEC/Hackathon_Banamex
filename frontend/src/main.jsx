374dccfbe99694d9cca133bc6540a68d67b18fbb
RoSosaTec <A01027913@tec.mx>
2026-05-18 14:41:37 -0600

    fix: Rename

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Dashboard from './Dashboard.jsx'
+import Encuesta from './Encuesta.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
